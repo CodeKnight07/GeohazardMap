b9be4defc62e6714ea02b334bd3d98174607143f
CodingKnight08 <macedajayson11@gmail.com>
2023-05-02 16:49:00 +0800

    create landing page navbar

--- geomap-frontend/pages/components/user/landing-page/landing-nav.tsx
+++ geomap-frontend/pages/components/user/landing-page/landing-nav.tsx
@@ -1,23 +1,26 @@
 import Image from 'next/image';
 import React, { useEffect, useState } from 'react';
-
+import {MdArrowForwardIos} from 'react-icons/md'
 function LandingNav() {
     const [isSmallDevice, setIsSmallDevice] = useState(false);
     const [isMobile, setIsMobile] = useState(false);
-  const [isTablet, setIsTablet] = useState(false);
-  const [isDesktop, setIsDesktop] = useState(false);
+    const [isTablet, setIsTablet] = useState(false);
+    const [isDesktop, setIsDesktop] = useState(false);
+
+
+ 
 
   useEffect(() => {
     function handleResize() {
       const screenWidth = window.innerWidth;
 
-      if (screenWidth <=480 ){
+      if (screenWidth <=640 ){
         setIsSmallDevice(true)
         setIsMobile(false);
         setIsTablet(false);
         setIsDesktop(false);
       }
-      else if (480<=screenWidth && screenWidth <= 768) {
+      else if (640<=screenWidth && screenWidth <= 768) {
         setIsSmallDevice(false)
         setIsMobile(true);
         setIsTablet(false);
@@ -43,22 +46,23 @@ function LandingNav() {
   }, []);
 
   return (
-    <nav className='bg-white'>
-        {isSmallDevice &&(
-            <div className='bg-black w-screen h-16 flex flex-col justify-center items-center'>
-                
-            </div>
-        )}
+    <nav className="fixed bg-gradient-to-br from-green-500 via-blue-400 to-purple-500">
+      {isSmallDevice && (
+        <div className=" w-screen h-16 flex flex-row justify-start items-center">
+          <div className=" w-3/5 h-full flex flex-row justify-center items-center">
+            <div>Company Name</div>
+          </div>
+          <div className=" w-36 h-auto flex flex-row justify-end items-start">
+            <button className="bg-green-500 px-3 py-1 rounded-md text-sm hover:bg-emerald-700" >
+              Sign In
+            </button>
+          </div>
+        </div>
+      )}
 
-      {isMobile && (
-        <div>Mobile Navigation</div>
-      )}
-      {isTablet && (
-        <div>Tablet Navigation</div>
-      )}
-      {isDesktop && (
-        <div>Desktop Navigation</div>
-      )}
+      {isMobile && <div></div>}
+      {isTablet && <div>Tablet Navigation</div>}
+      {isDesktop && <div>Desktop Navigation</div>}
     </nav>
   );
   }    
