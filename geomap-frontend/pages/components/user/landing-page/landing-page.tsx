94806578f2f2a57f93809d047af889dd3670badf
CodeKnight07 <ck3256504@gmail.com>
2023-05-04 05:47:06 +0800

    modified landing-page

--- geomap-frontend/pages/components/user/landing-page/landing-page.tsx
+++ geomap-frontend/pages/components/user/landing-page/landing-page.tsx
@@ -1,14 +1,21 @@
 import React from 'react'
 import LandingNav from './landing-nav'
+<<<<<<< Updated upstream
 import SideNav from './sidenav'
 import LandingContent from '../content/landing-content'
+=======
+>>>>>>> Stashed changes
 
 function LandingPage() {
   return (
     <div className='w-screen h-screen'>
         <LandingNav/>
+<<<<<<< Updated upstream
         <LandingContent/>
         <SideNav/>
+=======
+
+>>>>>>> Stashed changes
     </div>
   )
 }
