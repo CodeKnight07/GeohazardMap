bf0e7f91d146df78c94f4c5183dffe441069710d
CodingKnight08 <macedajayson11@gmail.com>
2023-05-03 09:40:25 +0800

    modify sidenav

--- geomap-frontend/pages/components/user/landing-page/sidenav.tsx
+++ geomap-frontend/pages/components/user/landing-page/sidenav.tsx
@@ -36,40 +36,40 @@ function SideNav() {
     <>
       {sideNav && (
         <div
-          className={`fixed top-20 left-4 transition-all duration-300 mx-0${
-            isVisible ? "translate-x-0" : "-translate-x-2"
+          className={`fixed top-20 left-1 transition-all duration-300 mx-0${
+            isVisible ? "translate-x-0" : "-translate-x-20"
           }`}
         >
           <div className=" w-auto h-1/2">
             {isIconClicked ? (
               <TfiArrowCircleRight
-                className="cursor-pointer"
-                size={16}
+                className="cursor-pointer text-white"
+                size={18}
                 onClick={toggleVisibility}
               />
             ) : (
               <TfiArrowCircleLeft
-                className="cursor-pointer"
-                size={16}
+                className="cursor-pointer text-white"
+                size={18}
                 onClick={toggleVisibility}
               />
             )}
             {isVisible && (
               <div className="bg-black p-4 rounded-lg h-1/2">
-                <ul className="flex flex-col justify-start items-center h-80 py-3 gap-6">
+                <ul className="flex flex-col justify-start items-center h-72 py-3 gap-6">
                   <li className="">
                     <a href="#">
-                      <FaHome className="text-white" />
+                      <FaHome className="text-white hover:text-yellow-400" />
                     </a>
                   </li>
                   <li className="">
                     <a href="#">
-                      <MdPersonSearch className="text-white" />
+                      <MdPersonSearch className="text-white hover:text-yellow-400" />
                     </a>
                   </li>
                   <li className="">
                     <a href="#">
-                      <FaQuestionCircle className="text-white" />
+                      <FaQuestionCircle className="text-white hover:text-yellow-400" />
                     </a>
                   </li>
                 </ul>
