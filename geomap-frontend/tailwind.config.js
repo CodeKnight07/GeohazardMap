6e02e5af29b0cd4bf2c8f8951817873df29c4def
CodeKnight07 <ck3256504@gmail.com>
2023-05-06 00:08:22 +0800

    modify tailwind config

--- geomap-frontend/tailwind.config.js
+++ geomap-frontend/tailwind.config.js
@@ -12,6 +12,22 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors:{
+        'royal-blue': {
+          '50': '#f0f5fe',
+          '100': '#dee8fb',
+          '200': '#c4d8f9',
+          '300': '#9bbef5',
+          '400': '#6c9cee',
+          '500': '#3d70e6',
+          '600': '#345cdc',
+          '700': '#2b49ca',
+          '800': '#293ca4',
+          '900': '#263782',
+          '950': '#1c244f',
+      },
+      
+      }
     },
   },
   plugins: [],
