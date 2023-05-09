510a05d4787c85dd54371037539b830414c0cc10
CodeKnight07 <ck3256504@gmail.com>
2023-05-09 22:21:24 +0800

    enhance user-dashboard-nav

--- geomap-frontend/pages/components/user/user-dashboard/dashboard-nav.tsx
+++ geomap-frontend/pages/components/user/user-dashboard/dashboard-nav.tsx
@@ -1,24 +1,30 @@
 import { useEffect, useState } from "react";
 import React from "react";
-import { FaBars, FaTimes, FaHome, FaUser, FaCog, FaSignOutAlt } from 'react-icons/fa';
+import { FaBars, FaTimes, FaHome, FaUser, FaCog, FaSignOutAlt, FaMapMarkedAlt, FaBook, FaHistory } from 'react-icons/fa';
 import {SlArrowDown} from 'react-icons/sl'
+import {MdPermContactCalendar} from 'react-icons/md'
 function UserDashboardNav () {
   const [smallDevice, setSmallDevice] = useState(false);
   const [showSidebar, setShowSidebar] = useState(false);
   const [showDropdown, setShowDropdown] = useState(false);
 
+
+  const handleSidebar = () => {
+    setShowSidebar(!showSidebar);
+  };
+
   const handleScreen = () => {
     const screenWidth = window.innerWidth;
     if (screenWidth < 768) {
       setSmallDevice(true);
+      handleSidebar()
+      
     } else {
       setSmallDevice(false);
     }
   };
 
-  const handleSidebar = () => {
-    setShowSidebar(!showSidebar);
-  };
+
 
   const handleDropdown = () => {
     setShowDropdown(!showDropdown);
@@ -47,10 +53,46 @@ function UserDashboardNav () {
           )}
         </div>
       ) : (
-        <div>Large Device View</div>
+        <div className="fixed top-20 left-0 w-40 h-screen bg-slate-800 flex flex-col justify-start items-center">
+          <ul className="flex flex-col space-y-3">
+            <li className="text-white text-xl flex flex-row items-center space-x-2">
+              <FaHome/>
+              <a href="#" className="border-collapse hover:border-b-2 border-royal-blue-400 ">Home</a>
+            </li>
+              <li>
+              <div className="flex flex-row items-center space-x-2">
+               <FaMapMarkedAlt className="text-white"/> <span className="text-xl flex-row items-center space-x-1 w-auto text-white pr-1 border-collapse hover:border-b border-royal-blue-400">
+                  Maps 
+                  
+                  </span><SlArrowDown onClick={handleDropdown} className="text-white"/>
+              
+                  
+                
+              </div>
+              {showDropdown && (
+                <ul className=" ml-5">
+                  <li className="text-white border-collapse hover:border-b border-royal-blue-400"><a href="#">Landslide</a></li>
+                  <li className="text-white border-collapse hover:border-b border-royal-blue-400"><a href="#">Flood</a></li>
+                  <li className="text-white border-collapse hover:border-b border-royal-blue-400"><a href="#">Evacuation</a></li>
+                </ul>
+              )}
+            </li>
+            <li className="text-white text-xl flex flex-row items-center space-x-2"><FaHistory/><a href="#" className="border-collapse hover:border-b-2 border-royal-blue-400">History</a></li>
+            <li className="text-white text-xl flex flex-row items-center space-x-2"><MdPermContactCalendar/><a href="#" className="border-collapse hover:border-b-2 border-royal-blue-400">Contacts</a></li>
+            <li className="text-white text-xl flex flex-row items-center space-x-2"><FaBook/><a href="#" className="border-collapse hover:border-b-2 border-royal-blue-400">Guidelines</a></li>
+           
+
+          </ul>
+        </div>
       )}
+
+
+
+
+
+      {/* For small screen */}
       {showSidebar && (
-        <div className="fixed top-20 left-0 w-1/3 h-5/6 bg-gradient-to-b from-white via-cyan-100 to-green-600 z-50 flex flex-col items-center">
+        <div className="fixed top-20 left-0 w-1/3 h-5/6 bg-slate-700 z-50 flex flex-col items-center">
           <button
             className="text-2xl absolute top-3 right-3"
             onClick={handleSidebar}
@@ -62,25 +104,25 @@ function UserDashboardNav () {
 
           </div>
           <ul className="mt-8 flex flex-col space-y-4">
-            <li className="text-lg border-collapse hover:border-b border-royal-blue-400"><a href="#">Home</a></li>
+            <li className="text-lg text-white border-collapse hover:border-b border-royal-blue-400 flex flex-row items-center space-x-2"><FaHome/><a href="#">Home</a></li>
             <li>
-              <div className="flex items-center justify-between">
-                <span className="text-lg pr-1 border-collapse hover:border-b border-royal-blue-400">Maps</span>
-                <button onClick={handleDropdown}>
-                  <SlArrowDown/>
-                </button>
+              <div className="text-white flex items-center justify-between space-x-1">
+              <FaMapMarkedAlt/>  <span className="text-lg pr-1 border-collapse hover:border-b border-royal-blue-400">Maps</span>
+               
+                  <SlArrowDown onClick={handleDropdown}/>
+                
               </div>
               {showDropdown && (
-                <ul className="ml-4">
+                <ul className="ml-4 text-neutral-300">
                   <li className="border-collapse hover:border-b border-royal-blue-400"><a href="#">Landslide</a></li>
                   <li className="border-collapse hover:border-b border-royal-blue-400"><a href="#">Flood</a></li>
                   <li className="border-collapse hover:border-b border-royal-blue-400"><a href="#">Evacuation</a></li>
                 </ul>
               )}
             </li>
-            <li className="text-lg border-collapse hover:border-b border-royal-blue-400"><a href="#">History </a></li>
-            <li className="text-lg border-collapse hover:border-b border-royal-blue-400"><a href="#"> Contacts </a></li>
-            <li className="text-lg border-collapse hover:border-b border-royal-blue-400"><a href="#">Guidelines</a></li>
+            <li className="text-lg text-white border-collapse hover:border-b border-royal-blue-400 flex flex-row items-center space-x-2"><FaHistory/><a href="#">History </a></li>
+            <li className="text-lg  text-white border-collapse hover:border-b border-royal-blue-400 flex flex-row items-center space-x-2"><MdPermContactCalendar/><a href="#"> Contacts </a></li>
+            <li className="text-lg  text-white border-collapse hover:border-b border-royal-blue-400 flex flex-row items-center space-x-2"><FaBook/><a href="#">Guidelines</a></li>
           </ul>
         </div>
       )}
