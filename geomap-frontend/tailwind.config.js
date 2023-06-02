1634aeca6986144a17520e98e547684547826f90
changchangGSHV <GrytzhavavesValmoria@gmail.com>
2023-06-02 12:55:00 +0800

    add gold color in tailwind.js

--- geomap-frontend/tailwind.config.js
+++ geomap-frontend/tailwind.config.js
@@ -32,6 +32,10 @@ module.exports = {
           '900': '#263782',
           '950': '#1c244f',
       },
+      gold: {
+        light: '#FFD700', // Gold
+        dark: '#B8860B',    // Dark goldenrod
+      },
       
         primary: {"50":"#eff6ff","100":"#dbeafe","200":"#bfdbfe","300":"#93c5fd","400":"#60a5fa","500":"#3b82f6","600":"#2563eb","700":"#1d4ed8","800":"#1e40af","900":"#1e3a8a"}
       
